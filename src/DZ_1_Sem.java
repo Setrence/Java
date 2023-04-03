c97b3ac7be2f7df69bf65fc965b856878eac0611
CrazyBlackFire <cxdbfxbn@mail.ru>
2023-04-03 14:20:29 +0300

    complite commit

--- src/DZ_1_Sem.java
+++ src/DZ_1_Sem.java
@@ -1,17 +1,62 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Objects;
+import java.util.Scanner;
+public class DZ_1_Sem {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Задача №1
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите число N: ");
+//        int n = in.nextInt();
+//        int k = 0;
+//        int f = 1;
+//            for (int i = 0; i <= n; i++) {
+//                k = k + i;
+//            }
+//            for (int i = 1; i <= n; i++) {
+//                f = f * i;
+//            }
+//        System.out.printf("Сумма чисел от 1 до N равна: %d\n", k);
+//        System.out.printf("Произведение чисел от 1 до N равна: %d", f);
+//        in.close();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//        Задача №2
+//        int n = 1000;
+//        int count = 0;
+//        System.out.print("2, ");
+//            for (int i = 1; i <= n; i++){
+//                for (int j = 1; j <= n; j++) {
+//                    int k = i % j;
+//                    if (k == 0) {
+//                        count += 1;
+//                    }
+//                }
+//                if (count == 2) {
+//                    System.out.printf("%d, ", i);
+//                } else if (count > 2){
+//                    count = 0;
+//                }
+//
+//            }
+
+//        Задача № 3
+//        double Otvet = 0;
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите первое число: ");
+//        int number_1 = in.nextInt();
+//        System.out.print("Введите знак: ");
+//        String znak = in.next();
+//        System.out.print("Введите второе число: ");
+//        int number_2 = in.nextInt();
+    //        if (Objects.equals(znak, "/")) {
+    //            Otvet = (double) number_1 / number_2;
+    //        } else if (Objects.equals(znak, "*")) {
+    //            Otvet = (double) number_1 * number_2;
+    //        }else if (Objects.equals(znak, "+")) {
+    //            Otvet = (double) number_1 + number_2;
+    //        }else if (Objects.equals(znak, "-")) {
+    //            Otvet = (double) number_1 - number_2;
+    //        }
+//        System.out.printf("Ответ = %.2f",Otvet);
+//        in.close();
     }
 }
